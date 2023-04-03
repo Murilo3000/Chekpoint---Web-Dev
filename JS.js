1ad5e3be37876610c501f017486dca743881d340
Logon Aluno <logonrmlocal@fiap.com.br>
2023-04-03 20:31:38 -0300

    Quarto commit - Murilo

--- JS.js
+++ JS.js
@@ -5,3 +5,16 @@ console.log(varUnd)
 //2 -Como usar o operador % em JavaScript? Exemplifique e demonstre a saída! (5 Pontos)
 let operador = 2;
 console.log(operador%2)
+
+//3 -Escreva um programa que calcula o índice de massa corporal (IMC) de uma pessoa e exibe uma mensagem indicando se ela está abaixo, no peso ideal ou acima do peso. Considere as seguintes faixas de IMC:  abaixo de 18,5 (abaixo do eso), entre 18,5   24,9 (peso ideal) e acima de 24,9 (acima do peso). Exemplifique e demonstre a saída! (10 Pontos)
+let peso = prompt("Digite seu peso: ")
+let altura = prompt("Digite sua altura")
+let alturaCalculo = altura*altura;
+
+if (peso/alturaCalculo < 18,5){
+    console.log("Você está abaixo do IMC recomendado")
+} if (peso/alturaCalculo >= 18.5 && peso/alturaCalculo <= 24.9){
+    console.log("Você está entre a média do IMC recomendado")
+} if (peso/alturaCalculo > 24.9) {
+    console.log("Você está acima do IMC recomendado")
+}
