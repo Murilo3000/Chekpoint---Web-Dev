3176c37ce6badd3cb1f8dc115643658a92b67bfd
Logon Aluno <logonrmlocal@fiap.com.br>
2023-04-03 20:08:13 -0300

    Terceiro commit - Murilo

--- JS.js
+++ JS.js
@@ -1,2 +1,7 @@
+//1 -Como declarar uma variável com valor undefined em JavaScript? Exemplifique e demonstre a saída do valor da variável! (5 Pontos) 
 let varUnd;
 console.log(varUnd)
+
+//2 -Como usar o operador % em JavaScript? Exemplifique e demonstre a saída! (5 Pontos)
+let operador = 2;
+console.log(operador%2)
