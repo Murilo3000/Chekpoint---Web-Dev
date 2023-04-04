83642db86b2b948f187c07b47206109a905c2bdf
Lucas Sobral <sobralroxo@gmail.com>
2023-04-04 11:11:12 -0300

    Exercício 4 - Lucas Sobral

--- JS.js
+++ JS.js
@@ -18,3 +18,16 @@ if (peso/alturaCalculo < 18,5){
 } if (peso/alturaCalculo > 24.9) {
     console.log("Você está acima do IMC recomendado")
 }
+
+//4 -Faça um programa que receba a idade de uma pessoa e exiba uma mensagem indicando em qual faixa etária ela se encontra: criança (até 12 anos), adolescente (entre 13 e 18 anos), adulto (entre 19 e 60 anos) ou idoso (mais de 60 anos). Exemplifique e demonstre a saída! (10 Pontos)
+let idade = prompt("Digite a sua idade: ")
+
+if (idade <= 12){
+    document.write("Com " + idade + " anos, você se enquadra como: criança.")
+} else if (idade <= 18){
+    document.write("Com " + idade + " anos, você se enquadra como: adolescente.")
+} else if (idade <= 60){
+    document.write("Com " + idade + " anos, você se enquadra como: adulto.")
+} else {
+    document.write("Com " + idade + " anos, você se enquadra como: idoso.")
+}
